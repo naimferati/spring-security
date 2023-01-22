2ae88484ee97ed457e4192b5d83e2fe0248b5825
Naim <naimferati97@gmail.com>
2023-01-22 23:25:36 +0100

    User Roles and Authorities (Permission) - Added users - Added Roles and Privileges as Enums - Secured the /api/** endpoints to be accessed only by users with role STUDENT

--- src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,17 +1,32 @@
 package com.example.springsecurity.security;
 
 import jakarta.servlet.DispatcherType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static com.example.springsecurity.security.ApplicationUserRole.ADMIN;
+import static com.example.springsecurity.security.ApplicationUserRole.STUDENT;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     //We cannot extend the WebSecurityConfigurerAdapter because it is deprecated, so we have to do in this way!
 
@@ -23,8 +38,8 @@ public class ApplicationSecurityConfig {
                 .authorizeHttpRequests((authz) -> authz
                         .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
                         //requestMatcher is the same as antMatcher (antMatcher was the name in previous versions)
-                        .requestMatchers("/", "/index.html", "/css/*", "/js/*")
-                        .permitAll()
+                        .requestMatchers("/", "/index.html", "/css/*", "/js/*").permitAll()
+                        .requestMatchers("/api/**").hasRole(STUDENT.name())
                         .anyRequest()
                         .authenticated()
                 )
@@ -41,6 +56,28 @@ public class ApplicationSecurityConfig {
                 .httpBasic();*/
 
         return http.build();
+    }
+
+
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails bejtullaFeratiUser = User.builder()
+                .username("bejtullaferati")
+                .password(passwordEncoder.encode("password"))
+                .roles(STUDENT.name()) // Spring uses this as ROLE_STUDENT
+                .build();
+
+        UserDetails naimFeratiUser = User.builder()
+                .username("naimferati")
+                .password(passwordEncoder.encode("password123"))
+                .roles(ADMIN.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                bejtullaFeratiUser,
+                naimFeratiUser
+
+        );
     }
 
 }
